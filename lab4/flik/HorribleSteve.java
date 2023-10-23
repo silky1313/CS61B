e5da339d3985886375b1726da072a64dd08a9ec6
silky1313 <qq2623153066@outlook.com>
2023-10-23 20:02:47 +0800

    success lab4

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -3,6 +3,7 @@ package flik;
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
+        /**采用断电测试后发现在i==j=128的时候if里面的条件是false*/
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
